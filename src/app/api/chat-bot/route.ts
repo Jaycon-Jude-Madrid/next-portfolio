c24c8516843a454c23c65d3e14e42eea59fb0dbe
Jaycon46 <jaycon@itsjobvious.com>
2025-02-20 01:40:18 +0800

    .

--- src/app/api/chat-bot/route.ts
+++ src/app/api/chat-bot/route.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextRequest, NextResponse } from "next/server";
 
-const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 if (!apiKey) {
 	throw new Error("GEMINI_API_KEY is not defined");
 }
