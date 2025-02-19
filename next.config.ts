2ec1ef880eb63f920d6574a7f24a1cf6b6c01d15
Jaycon46 <jaycon@itsjobvious.com>
2025-02-20 04:04:37 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
 	images: {
 		unoptimized: true,
 	},
+	env: {
+		NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+		NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+	},
+
 	/* config options here */
 };
 
