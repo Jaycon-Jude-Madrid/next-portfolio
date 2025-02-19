b81608cbeed4d3a1af349d72f31e86f3eae6b457
Jaycon46 <jaycon@itsjobvious.com>
2025-02-20 03:35:01 +0800

    .

--- src/app/api/chat-bot/route.ts
+++ src/app/api/chat-bot/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY || "";
 
-console.log(apiKey);
+// console.log(apiKey);
 if (!apiKey) {
 	throw new Error("GEMINI_API_KEY is not defined");
 }
